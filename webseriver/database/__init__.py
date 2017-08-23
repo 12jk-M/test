3bdb7abb56ee545a89f92763f65d24853b7a93f5
12jk-M <1244907347@qq.com>
2017-08-23 13:43:54 +0800

    Signed-off-by: 12jk-M <1244907347@qq.com>

--- webseriver/database/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 23 15:08:19 2016
-##database
-@author: lywen
-"""
-
--- webseriver/help/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 23 15:20:08 2016
-help
-@author: lywen
-"""
-
